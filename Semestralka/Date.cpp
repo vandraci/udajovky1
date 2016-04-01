6513ac53ca30e7d879025cb227813924c45b7eb4
vandraci <vandraci@centrum.sk>
2016-04-01 14:05:56 +0200

    Update Date.cpp

--- Semestralka/Date.cpp
+++ Semestralka/Date.cpp
@@ -2,11 +2,20 @@
 
 
 
-Date::Date()
+Date::Date(int d, int m, int y)
 {
+	this->day = d;
+	this->month = m;
+	this->year = y;
 }
 
 
 Date::~Date()
 {
+}
+
+int Date::difference(Date d1, Date d2)
+{
+	int pom = (d1.day - d2.day) + ((d1.month - d2.month) * 30) + ((d1.year - d2.year) * 365);
+	return pom;
 }
